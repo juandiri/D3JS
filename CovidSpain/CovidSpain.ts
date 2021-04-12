1969d045c362a7a8cfcf4a7c7f40268ee49018b4
juandiri <juandiri98@gmail.com>
2021-04-12 19:48:52 +0200

    Spain map with covid cases with button (Problem in March)

--- CovidSpain/CovidSpain.ts
+++ CovidSpain/CovidSpain.ts
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import { on } from "node:events";
 import * as topojson from "topojson-client";
 const spainjson = require("./spain.json");
-import {stats} from "./statsspain";
+import {covidCasesMarch2020, covidCasesApril2021,ResultEntry} from "./CovidData";
 const d3Composite = require("d3-composite-projections");
 import {latLongCommunities} from './communities'
 import { stat } from "node:fs";
@@ -20,7 +20,7 @@ const color = d3
     "#540000",
   ]);
 
-const maxAffected = stats.reduce(
+const maxAffected = covidCasesApril2021.reduce(
     (max, item) => (item.value > max ? item.value : max),
     0
   );
@@ -30,14 +30,14 @@ const affectedRadiusScale = d3
   .domain([0,maxAffected])
   .range([0,50]);
 
-  const calculateRadiusBasedOnAffectedCases = (comunidad: string) => {
-    const entry = stats.find((item) => item.name === comunidad);
-  
+  const calculateRadiusBasedOnAffectedCases = (comunidad: string, data: ResultEntry[]) => {
+
+    const entry = data.find((item) => item.name === comunidad);
     return entry ? affectedRadiusScale(entry.value) : 0;
   };
 
-  const obtainAffectedCases = (comunidad: string) => {
-    const entry = stats.find((item) => item.name === comunidad);
+  const obtainAffectedCases = (comunidad: string, data:ResultEntry[] ) => {
+    const entry = data.find((item) => item.name === comunidad);
   
     return entry ? entry.value : 0;
   };
@@ -63,7 +63,9 @@ const svg = d3
   .append("svg")
   .attr("width", 1024)
   .attr("height", 800)
-  .attr("style", "background-color: #FBFAF0");
+  .attr("style", "background-color: #FBFAF0")
+  ;
+
 
 svg
   .selectAll("path")
@@ -71,12 +73,24 @@ svg
   .enter()
   .append("path")
   .attr("class", "country")
-  .attr("d", geoPath as any)
+  // data loaded from json file
+  .attr("d", geoPath as any);
+ 
+const updateMap = (data: ResultEntry[]) => {
+      svg.selectAll("circle").remove();
+  return svg
+  .selectAll("circle")
+  .data(latLongCommunities)
+  .enter()
+  .append("circle")
+  .attr("class", "affected-marker")
+  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name, data))
+  .attr("cx", d => aProjection([d.long, d.lat])[0])
+  .attr("cy", d => aProjection([d.long, d.lat])[1])
   .on("mouseover", function (e: any, datum: any) {
-    console.log(datum);
     d3.select(this).attr("transform", "");
-    const CCAA = datum.properties.NAME_1;
-    const cases = obtainAffectedCases(CCAA);
+    const CCAA = datum.name;
+    const cases = obtainAffectedCases(CCAA, data);
     const coords = { x: e.x, y: e.y };
     div.transition().duration(200).style("opacity", 0.9);
     div
@@ -88,13 +102,19 @@ svg
     d3.select(this).attr("transform", "");
     div.transition().duration(500).style("opacity", 0);
   });
+  }
 
-svg
-  .selectAll("circle")
-  .data(latLongCommunities)
-  .enter()
-  .append("circle")
-  .attr("class", "affected-marker")
-  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name))
-  .attr("cx", d => aProjection([d.long, d.lat])[0])
-  .attr("cy", d => aProjection([d.long, d.lat])[1]);
+  document
+  .getElementById("March 2021")
+  .addEventListener("click", function handlResultsApril() {
+    console.log("MArch")
+    updateMap(covidCasesMarch2020);
+  });
+
+document
+  .getElementById("April 2021")
+  .addEventListener("click", function handlResultsNovember() {
+    console.log("april")
+    updateMap(covidCasesApril2021);
+  });
+  

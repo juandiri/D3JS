f180aca9cecc0b86af39cb968b9f6ba16853c43f
juandiri <juandiri98@gmail.com>
2021-04-16 19:57:49 +0200

    2nd version of solution and data updated

---
+++ CovidSpain/CovidData.ts
@@ -0,0 +1,138 @@
+export interface ResultEntry {
+    name: string;
+    value: number;
+  }
+
+  export const covidCasesMarch2020: ResultEntry[] = [
+    {
+      name: "Comunidad de Madrid",
+      value: 174,
+    },
+    {
+      name: "La Rioja",
+      value: 39,
+    },
+    {
+      name: "Andalucía",
+      value: 34,
+    },
+    {
+      name: "Cataluña",
+      value: 24,
+    },
+    {
+      name: "Comunidad Valenciana",
+      value: 30,
+    },
+    {
+      name: "Región de Murcia",
+      value: 3,
+    },
+    {
+      name: "Extremadura",
+      value: 6,
+    },
+    {
+      name: "Castilla-La Mancha",
+      value: 16,
+    },
+    {
+      name: "País Vasco",
+      value: 45,
+    },
+    {
+      name: "Cantabria",
+      value: 10,
+    },
+    {
+      name: "Principado de Asturias",
+      value: 5,
+    },
+    {
+      name: "Galicia",
+      value: 3,
+    },
+    {
+      name: "Aragón",
+      value: 11,
+    },
+    {
+      name: "Castilla y León",
+      value: 19,
+    },
+    {
+      name: "Islas Canarias",
+      value: 18,
+    },
+    {
+      name: "Islas Baleares",
+      value: 6,
+    },
+  ];
+
+  export const covidCasesApril2021: ResultEntry[] = [
+    {
+      name: "Comunidad de Madrid",
+      value: 640656,
+    },
+    {
+      name: "La Rioja",
+      value: 28340,
+    },
+    {
+      name: "Andalucía",
+      value: 518285,
+    },
+    {
+      name: "Cataluña",
+      value: 547239,
+    },
+    {
+      name: "Comunidad Valenciana",
+      value: 387107,
+    },
+    {
+      name: "Región de Murcia",
+      value: 109243,
+    },
+    {
+      name: "Extremadura",
+      value: 72055,
+    },
+    {
+      name: "Castilla-La Mancha",
+      value: 178092,
+    },
+    {
+      name: "País Vasco",
+      value: 168334,
+    },
+    {
+      name: "Cantabria",
+      value: 26653,
+    },
+    {
+      name: "Principado de Asturias",
+      value: 48329,
+    },
+    {
+      name: "Galicia",
+      value: 117841,
+    },
+    {
+      name: "Aragón",
+      value: 112848,
+    },
+    {
+      name: "Castilla y León",
+      value: 215845,
+    },
+    {
+      name: "Islas Canarias",
+      value: 48788,
+    },
+    {
+      name: "Islas Baleares",
+      value: 58153,
+    },
+  ];
